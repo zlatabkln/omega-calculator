f3aed765087a3b3e614127455e71c3c7be881fdf
zlatabkln <60300554+zlatabkln@users.noreply.github.com>
2022-12-17 18:32:04 +0200

    Update my_lexer.py

--- my_lexer.py
+++ my_lexer.py
@@ -1,9 +1,10 @@
-import grammar
-import my_exception
-import my_token
+from grammar import *
+from my_exception import *
+from my_token import *
 
 
-class Lexer:
+# lexer will convert string expression to the list of tokens - object that'll represent char as type and, if it's a number, value
+class MyLexer:
     def __init__(self, expression):
         self.expression = expression  # given expression
         self.pos = -1  # current position
@@ -11,6 +12,12 @@ class Lexer:
         self.prev_char = None  # char on previous position (for minus token)
         self.get_next()  # goes to the next character in expression
 
+    # returns the value of the next char if exists
+    def check_next(self):
+        if self.pos + 1 < len(self.expression):
+            return self.expression[self.pos + 1]
+
+    # goes to the next char if exists
     def get_next(self):
         self.pos += 1
         self.prev_char = self.curr_char
@@ -23,10 +30,10 @@ class Lexer:
     def make_number(self, sign):
         num_str = ""
         dots = 0
-        while self.curr_char is not None and (self.curr_char in grammar.DIGITS or self.curr_char == '.'):
+        while self.curr_char is not None and (self.curr_char in DIGITS or self.curr_char == '.'):
             if self.curr_char == '.':
                 if dots != 0:
-                    raise my_exception.SyntaxException("dots")
+                    raise SyntaxException("dots")
                 else:
                     if num_str == " " or num_str == "-":
                         num_str += '0'
@@ -45,28 +52,30 @@ class Lexer:
     # make_token_from_num - gets a number(int or float) and returns a token matching number's type
     def make_token_from_num(self, num):
         if type(num) == int:
-            t_type = 'TT_INT'
+            t_type = 'INT'
         else:
-            t_type = 'TT_FLOAT'
-        return my_token.Token(t_type, num, 0, "any")
+            t_type = 'FLOAT'
+        return Token(t_type, num, 0)
 
     # token_minus - counts minuses and optimises to final operator - + or -, creates matching token
-    def token_minus(self):
+    def token_minus(self, tokens):
         count = 0
         while self.curr_char is not None and self.curr_char == '-':
-            if count == 0 and self.is_sign():
+            if self.prev_char is None and self.check_next() not in DIGITS:
+                tokens.append(Token('INT', 0, 0))
+            elif count == 0 and self.is_sign():
                 self.get_next()
-                return self.make_number(-1)
+                return self.make_token_from_num(self.make_number(-1))
             count += 1
             self.get_next()
         if count % 2 == 0:
-            return my_token.Token('TT_ADD', '+', "1", "both")
+            return Token('ADD', None, 1)
         else:
-            return my_token.Token('TT_SUB', '-', "1", "both")
+            return Token('SUB', None, 1)
 
     # checks if minus is a sign or an operator
     def is_sign(self):
-        if self.prev_char in grammar.DIGITS or self.prev_char == ')' or self.prev_char in grammar.R_UN_OPERATORS:
+        if self.prev_char in DIGITS or self.prev_char == ')' or self.prev_char in R_UN_OPERATORS:
             return False
         else:
             return True
@@ -77,27 +86,21 @@ class Lexer:
         while self.curr_char is not None:
             if str(self.curr_char).isspace():  # if char is blanc character, get next character
                 self.get_next()
-            elif self.curr_char in grammar.MINUS:  # '-'
-                tokens.append(self.make_token_from_num(self.token_minus()))
-            elif self.curr_char in grammar.DIGITS or self.curr_char == '.':  # number
+            elif self.curr_char in MINUS:  # '-'
+                tokens.append(self.token_minus(tokens))
+            elif self.curr_char in DIGITS or self.curr_char == '.':  # number
                 tokens.append(self.make_token_from_num(self.make_number(1)))
             else:  # different operators
-                if any(self.curr_char in sublist for sublist in grammar.DU_OPERATORS.keys()):
-                    t_d = grammar.DU_OPERATORS.get(self.curr_char)
-                elif any(self.curr_char in sublist for sublist in grammar.L_UN_OPERATORS.keys()):
-                    t_d = grammar.L_UN_OPERATORS.get(self.curr_char)
-                elif any(self.curr_char in sublist for sublist in grammar.R_UN_OPERATORS.keys()):
-                    t_d = grammar.R_UN_OPERATORS.get(self.curr_char)
-                elif any(self.curr_char in sublist for sublist in grammar.PAR.keys()):
-                    t_d = grammar.PAR.get(self.curr_char)
+                if any(self.curr_char in sublist for sublist in DU_OPERATORS.keys()):
+                    t_d = DU_OPERATORS.get(self.curr_char)
+                elif any(self.curr_char in sublist for sublist in L_UN_OPERATORS.keys()):
+                    t_d = L_UN_OPERATORS.get(self.curr_char)
+                elif any(self.curr_char in sublist for sublist in R_UN_OPERATORS.keys()):
+                    t_d = R_UN_OPERATORS.get(self.curr_char)
+                elif any(self.curr_char in sublist for sublist in PAR.keys()):
+                    t_d = PAR.get(self.curr_char)
                 else:  # forbidden character
-                    raise my_exception.LexError(self.curr_char)
-                tokens.append(my_token.Token(t_d.get("type"), None, t_d.get("level"), t_d.get("side")))
+                    raise LexError(self.curr_char)
+                tokens.append(Token(t_d.get("type"), None, t_d.get("level")))
                 self.get_next()
         return tokens
-
-
-def run(text):
-    lex = Lexer(text)
-    tokens = lex.make_token_list()
-    return tokens
