e28a2a35af2c9695b1f2d3929d36aaafdd4bf77f
zlatabkln <60300554+zlatabkln@users.noreply.github.com>
2022-12-18 16:32:13 +0200

    Update my_token.py

--- my_token.py
+++ my_token.py
@@ -1,16 +1,16 @@
 class Token:
     # class of tokens, represented as type:value (numbers) or type (operators)
     # example 2+3->[TT_INT:2,TT_ADD,TT_INT:3]
-    def __init__(self, type_, value, level):
-        self.type = type_
+    def __init__(self, type_tok, value, level, func):
+        self.type = type_tok
         self.value = value
         self.level = level
+        self.func = func
 
     def __repr__(self):
-        if self.value is not None:
+        if self.type == 'INT' or self.type == 'FLOAT':
             return f'{self.type}:{self.value}'
         return f'{self.type}'
 
 
-def get_level(tok):
-    return tok.level
+
