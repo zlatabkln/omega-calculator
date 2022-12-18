518faf4bd50d08cb460ffd403bb5cfe283c15611
zlatabkln <60300554+zlatabkln@users.noreply.github.com>
2022-12-18 16:05:14 +0200

    Update node_classes.py

--- node_classes.py
+++ node_classes.py
@@ -9,6 +9,8 @@ binary operators, in unary operators - one of the sons will be None) - the opera
 
 class NumNode:
     def __init__(self, tok):
+        if tok.type not in ['INT', 'FLOAT']:
+            raise ValueError("Must be a number")
         self.tok = tok
 
     def __repr__(self):
@@ -19,13 +21,13 @@ class OpNode:
     def __init__(self, left, op_tok, right):
         if any(op_tok.type in subdict.values() for subdict in DU_OPERATORS.values()) or op_tok.type == 'SUB':
             if left is None or right is None:
-                raise SyntaxException("near operator")
+                raise SyntaxException("near operator", op_tok)
         elif any(op_tok.type in subdict.values() for subdict in L_UN_OPERATORS.values()):
             if left is not None or right is None:
-                raise SyntaxException("near operator")
+                raise SyntaxException("near operator", op_tok)
         else:
             if left is None or right is not None:
-                raise SyntaxException("near operator")
+                raise SyntaxException("near operator", op_tok)
         self.op_tok = op_tok
         self.right = right
         self.left = left
@@ -37,15 +39,3 @@ class OpNode:
             return f'({self.op_tok},{self.right})'
         else:
             return f'({self.left},{self.op_tok})'
-
-
-class UnOpNode:
-    def __init__(self, op_tok, node):
-        self.op_tok = op_tok
-        self.node = node
-
-    def __repr__(self):
-        if any(self.op_tok.type in subdict.values() for subdict in R_UN_OPERATORS.values()):
-            return f'({self.node},{self.op_tok})'
-        else:
-            return f'({self.op_tok},{self.node})'
