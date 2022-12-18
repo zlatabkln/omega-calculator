9b65fa03d3ca5385c17c3cd431b4ac83ff8281b5
zlatabkln <60300554+zlatabkln@users.noreply.github.com>
2022-12-18 16:54:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-import my_lexer
-import my_parser
-#test lexer
-print(my_lexer.run("(.2345)^2*-3"))
+import input_modul
+from my_lexer import *
+from my_parser import *
+from calculator import *
+
+
+def run(text):
+  #1.Lexer
+    lex = MyLexer(text) 
+    tokens = lex.make_token_list() 
+  #2.Parser
+    pars = MyParser(tokens)
+    tree = pars.parse()
+  #3.Calculation
+    res = calculate(tree)
+    return res
+
+
+print(run(input_modul.input_func()))
 
