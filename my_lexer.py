87ed6d159d96debd0eeb30392042d060acabcba9
zlatabkln <60300554+zlatabkln@users.noreply.github.com>
2022-12-18 16:19:09 +0200

    Update my_lexer.py

--- my_lexer.py
+++ my_lexer.py
@@ -2,8 +2,10 @@ from grammar import *
 from my_exception import *
 from my_token import *
 
+'''lexer will convert string expression to the list of tokens
+ - object that'll represent char as type and, if it's a number, value'''
 
-# lexer will convert string expression to the list of tokens - object that'll represent char as type and, if it's a number, value
+
 class MyLexer:
     def __init__(self, expression):
         self.expression = expression  # given expression
@@ -33,7 +35,7 @@ class MyLexer:
         while self.curr_char is not None and (self.curr_char in DIGITS or self.curr_char == '.'):
             if self.curr_char == '.':
                 if dots != 0:
-                    raise SyntaxException("dots")
+                    raise SyntaxException("dots", None)
                 else:
                     if num_str == " " or num_str == "-":
                         num_str += '0'
@@ -55,30 +57,38 @@ class MyLexer:
             t_type = 'INT'
         else:
             t_type = 'FLOAT'
-        return Token(t_type, num, 0)
+        return Token(t_type, num, 0, None)
 
-    # token_minus - counts minuses and optimises to final operator - + or -, creates matching token
+    ''' token_minus - counts minuses and optimises to final operator - + or -, creates matching token
+     if minus is a sign:
+       - if there were other minus operators in a row directly before him - create a token from them and exit
+       the function - the sign minus will be handled by himself in a different entrance
+       - if sign minus comes by himself (or we entered this function second time for him) - add to the
+       following number and return a number token'''
+
     def token_minus(self, tokens):
         count = 0
         while self.curr_char is not None and self.curr_char == '-':
             if self.prev_char is None and self.check_next() not in DIGITS:
-                tokens.append(Token('INT', 0, 0))
-            elif count == 0 and self.is_sign():
+                tokens.append(Token('INT', 0, 0, None))
+            elif self.is_sign():
+                if count != 0:
+                    break
                 self.get_next()
                 return self.make_token_from_num(self.make_number(-1))
             count += 1
             self.get_next()
         if count % 2 == 0:
-            return Token('ADD', None, 1)
+            return Token('ADD', self.curr_char, 1, valid_add)
         else:
-            return Token('SUB', None, 1)
+            return Token('SUB', self.curr_char, 1, valid_sub)
 
     # checks if minus is a sign or an operator
     def is_sign(self):
-        if self.prev_char in DIGITS or self.prev_char == ')' or self.prev_char in R_UN_OPERATORS:
-            return False
-        else:
+        if self.check_next() in DIGITS and self.prev_char not in DIGITS and self.prev_char != ')' and self.prev_char not in R_UN_OPERATORS:
             return True
+        else:
+            return False
 
     # create a list of tokens from the expression
     def make_token_list(self):
@@ -101,6 +111,6 @@ class MyLexer:
                     t_d = PAR.get(self.curr_char)
                 else:  # forbidden character
                     raise LexError(self.curr_char)
-                tokens.append(Token(t_d.get("type"), None, t_d.get("level")))
+                tokens.append(Token(t_d.get("type"), self.curr_char, t_d.get("level"), t_d.get("func")))
                 self.get_next()
         return tokens
