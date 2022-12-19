dde6ad89581b440595c7f080922c189e520c7f9e
zlatabkln <60300554+zlatabkln@users.noreply.github.com>
2022-12-19 15:22:02 +0200

    Update input_modul.py

--- input_modul.py
+++ input_modul.py
@@ -3,18 +3,18 @@ import grammar
 
 # The input module - gets input from user, deletes the blank characters and passes to the lexer
 
-def input_func():
+def input_func() -> str:
     try:
         print("This is omega calculator. Please, enter the expression:\n")
         input_from_console = input()
         return clear_expression(input_from_console)
     except EOFError:
         print("EOF in input")
-        exit(1)
+        exit("eof")
 
 
 # deletes the blank chars from the entered string
-def clear_expression(text):
+def clear_expression(text: str) -> str:
     for c in text:
         if c in grammar.BLANKS:
             text = text.replace(c, '')
