4fb3d468a383a5b684a1140d5e8589ca16d16dc1
zlatabkln <60300554+zlatabkln@users.noreply.github.com>
2022-12-19 15:21:21 +0200

    Update grammar.py

--- grammar.py
+++ grammar.py
@@ -1,25 +1,27 @@
+from validation import *
+
 # in this module is stored all the grammar of the calculator. for each operator we define priority level and operand
 # sides
-ADD = {"type": 'ADD', "level": 1}
-DIV = {"type": 'DIV', "level": 2}
-MUL = {"type": 'MUL', "level": 2}
-POW = {"type": 'POW', "level": 3}
-MODULO = {"type": 'MODUL', "level": 4}
-MAX = {"type": 'MAX', "level": 5}
-MIN = {"type": 'MIN', "level": 5}
-AVG = {"type": 'AVG', "level": 5}
-FACTORIAL = {"type": 'FACTORIAL', "level": 6}
-SUM_DIGITS = {"type": 'SUM_DIGITS', "level": 6}
-TILDA = {"type": 'TILDA', "level": 6}
-RP = {"type": 'RP', "level": 0}
-LP = {"type": 'LP', "level": 0}
-
+ADD = {"type": 'ADD', "level": 1, "func": valid_add}
+DIV = {"type": 'DIV', "level": 2, "func": valid_div}
+MUL = {"type": 'MUL', "level": 2, "func": valid_mul}
+POW = {"type": 'POW', "level": 3, "func": valid_pow}
+MODULO = {"type": 'MODUL', "level": 4, "func": valid_modulo}
+MAX = {"type": 'MAX', "level": 5, "func": valid_max}
+MIN = {"type": 'MIN', "level": 5, "func": valid_min}
+AVG = {"type": 'AVG', "level": 5, "func": valid_avg}
+FACTORIAL = {"type": 'FACTORIAL', "level": 6, "func": valid_factorial}
+SUM_DIGITS = {"type": 'SUM_DIGITS', "level": 6, "func": valid_sum_digits}
+TILDA = {"type": 'TILDA', "level": 6, "func": valid_tilda}
+RP = {"type": 'RP', "level": 0, "func": None}
+LP = {"type": 'LP', "level": 0, "func": None}
+BLANKS = [" ", '\v', '\t', '\r']
 DU_OPERATORS = {'+': ADD, '/': DIV, '*': MUL, '^': POW, '%': MODULO,
                 '$': MAX, '&': MIN, '@': AVG}
 R_UN_OPERATORS = {'!': FACTORIAL, '#': SUM_DIGITS}
 L_UN_OPERATORS = {'~': TILDA}
-UN_OPERATORS=[R_UN_OPERATORS,L_UN_OPERATORS]
+UN_OPERATORS = [R_UN_OPERATORS, L_UN_OPERATORS]
 MINUS = ['-']
 PAR = {'(': LP, ')': RP}
-DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-TOKENS = [DU_OPERATORS, R_UN_OPERATORS, L_UN_OPERATORS, MINUS, PAR, DIGITS, '.']
+DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']
+TOKENS = [DU_OPERATORS, R_UN_OPERATORS, L_UN_OPERATORS, MINUS, PAR, DIGITS]
