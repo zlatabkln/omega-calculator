1e680a9974044b1e58277ebf712f66bfd96a9ab8
zlatabkln <60300554+zlatabkln@users.noreply.github.com>
2022-12-19 15:30:20 +0200

    Update my_exception.py

--- my_exception.py
+++ my_exception.py
@@ -11,24 +11,28 @@ class LexError(Exception):
 
 # syntax error - forbidden characters order exception, has different cases
 class SyntaxException(Exception):
-    def __init__(self, case: str):
+    def __init__(self, case: str, op_tok=None):
         self.case = case
+        self.op_tok = op_tok
 
     def __str__(self):
         if self.case == "dots":  # example - 2..2
             return "More than one dot in float"
-        elif self.case == "near operator":  # example
-            return f'Invalid syntax near operator'
+        elif self.case == "near operator":  # example - 2~3
+            return f'Invalid syntax near operator {self.op_tok.value}'
+        elif self.case == "null parentheses":  # example - ()-2
+            return f'Null parentheses'
+        elif self.case == "rp":
+            return f'Unexpected right parentheses'
+        elif self.case == "lp":
+            return f'Missing right parentheses'
 
 
 # absent operator exception - has different cases (default - no operator in expression at all)
 # also raised when there is an absent right parentheses
 class AbsentOperatorException(Exception):
-    def __init__(self, message):
-        if message:
-            self.message = message
-        else:
-            self.message = 'There is no operator in expression'
+    def __init__(self):
+        self.message = 'There is no operator in expression'
 
     def __str__(self):
         return f'{self.message}'
